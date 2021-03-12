21ee1170d5930ef51db0219d3ff68c2a18e8328d
zli155 <zli155@jhu.edu>
2021-03-11 19:26:28 -0500

    add comment

--- multi_thread_sum.py
+++ multi_thread_sum.py
@@ -2,6 +2,7 @@ import concurrent.futures
 def range_sum(start, end):
     return sum(range(start, end+1))
 
+# using thread pool
 executor = concurrent.futures.ThreadPoolExecutor(max_workers=5)
 futures = {executor.submit(range_sum, i*100+1, (i+1)*100): i for i in range(10)}
 res = 0
